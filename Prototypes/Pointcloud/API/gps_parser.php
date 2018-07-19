64e99b82d00d18418a009e9711cc66e1eaac8062
Matt Filer <matthew.filer@hotmail.co.uk>
2018-07-19 16:17:14 +0100

    Nightly 19/07/18

--- Prototypes/Pointcloud/API/gps_parser.php
+++ Prototypes/Pointcloud/API/gps_parser.php
@@ -1,12 +1,11 @@
 <?php
-	header('Content-Type: application/json');
 	$json_data = array();
 
 	//SRT Parse (DJI Drone Data)
-	if (isset($_GET['srt'])) {
-		$raw_data = file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['srt'] . ".srt");
+	if (file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['video'] . ".csv") == "") {
+		$raw_data = file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['video'] . ".srt");
 		if ($raw_data == "") {
-			$raw_data = file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['srt'] . ".SRT");
+			$raw_data = file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['video'] . ".SRT");
 		}
 		$line_count = 0;
 		foreach(preg_split("/((\r?\n)|(\r\n?))/", $raw_data) as $line){
@@ -18,14 +17,15 @@
 		    		$GPS_VALUES[1], 
 		    		$GPS_VALUES[0], 
 		    		$GPS_VALUES[2], 
-		    		$BAROMETER_VALUE + 25, 
+		    		(string)($BAROMETER_VALUE + 25), 
 		    		(string)($line_count * 1000),
 		    		"90", 
 		    		"0", 
 		    		"0", 
 		    		"90", 
 		    		"0", 
-		    		"0");
+		    		"0",
+		    		"SRT");
 		    	array_push($json_data, $line_data);
 		    	$line_count++;
 		    }
@@ -33,26 +33,35 @@
 	}
 
 	//CSV Parse (Litchi Data)
-	if (isset($_GET['csv'])) {
-		$raw_data = file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['csv'] . ".csv");
+	if ($json_data == array()) {
+		$raw_data = file_get_contents("http://assets.artstation.mattfiler.co.uk/VIDEOS/" . $_GET['location'] . "/" . $_GET['video'] . ".csv");
 		$line_count = 0;
+		$previous_line = array();
 		foreach(preg_split("/((\r?\n)|(\r\n?))/", $raw_data) as $line){
 			if ($line_count > 0) {
 		    	$line_data_raw = explode(",", $line);
 		    	if ($line_data_raw[0] != null) {
+		    		if ($line_data_raw[37] == 0) {
+		    			continue; //Drone isn't recording, so this data isn't relevant to the video
+		    		}
+		    		if ((($previous_line[65] / 10)*-1) > 100 && (($line_data_raw[65] / 10)*-1) < -100) {
+		    			$line_data_raw[65] *= -1; //Correct 180 degree rotation bug (WIP)
+		    		}
 			    	$line_data_important = array(
 			    		$line_data_raw[0], 
 			    		$line_data_raw[1], 
 			    		"0", 
 			    		$line_data_raw[2], 
 			    		$line_data_raw[10], 
-			    		($line_data_raw[60] / 10)+90, 
-			    		($line_data_raw[61] / 10), 
-			    		($line_data_raw[62] / 10)*-1, 
-			    		($line_data_raw[63] / 10)+90, 
-			    		($line_data_raw[64] / 10), 
-			    		($line_data_raw[65] / 10)*-1);
+			    		(string)(($line_data_raw[60] / 10)+90), 
+			    		(string)($line_data_raw[61] / 10), 
+			    		(string)(($line_data_raw[62] / 10)*-1), 
+			    		(string)(($line_data_raw[63] / 10)+90), 
+			    		(string)($line_data_raw[64] / 10), 
+			    		(string)(($line_data_raw[65] / 10)*-1),
+			    		"CSV");
 			    	array_push($json_data, $line_data_important);
+		    		$previous_line = $line_data_raw;
 		    	}
 			}
 		    $line_count++;
@@ -71,8 +80,15 @@
 		DRONE YAW (deg),
 		GIMBAL PITCH (deg), 
 		GIMBAL ROLL (deg), 
-		GIMBAL YAW (deg)
+		GIMBAL YAW (deg),
+		DATA SOURCE
 	*/
-	$json_data = json_encode($json_data);
-	echo $json_data;
+	if ($json_data != array()) {
+		header('Content-Type: application/json');
+		echo json_encode($json_data); //Output data
+	}
+	else
+	{
+		header("HTTP/1.0 404 Not Found"); //No data found from request, hide the page.
+	}
 ?>
